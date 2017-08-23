bc78edb4dc108e61a86e6ef722391460053a2467
Arturs Tamanis <arturs.tamanis@gmail.com>
2017-08-23 01:06:44 +0100

    Configuration by boost_program_options

--- HttpWrapper/src/main.cpp
+++ HttpWrapper/src/main.cpp
@@ -13,31 +13,60 @@
 #include <boost/asio.hpp>
 #include "server.hpp"
 
-int main(int argc, char* argv[])
+// Program Options support
+#include <fstream>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+int main(int argc, char** argv)
 {
-  try
-  {
-    // Check command line arguments.
-    if (argc != 4)
-    {
-      std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 80 .\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 80 .\n";
-      return 1;
-    }
+	unsigned short port;
+	std::string host;
+	std::string docsRoot;
+	std::string configFile;
 
-    // Initialise the server.
-    http::server::server s(argv[1], argv[2], argv[3]);
+	po::options_description server_config("Data Provider options");
+	server_config.add_options()
+		("help,H", "Usage: reminder-http <address> <port> <doc_root>\n")
+		("port,p", po::value<unsigned short>(&port), "HTTP server port")
+		("host,h", po::value<std::string>(&host)->default_value("localhost"), "HTTP sever address")
+		("path,P", po::value<std::string>(&docsRoot), "Site root folder")
+		("config,C", po::value<std::string>(&configFile)->default_value("reminder-http.ini"), "Configuration file")
+	;
+	
+	po::variables_map config_vars;
+	po::store(po::parse_command_line(argc, argv, server_config), config_vars);
+	po::notify(config_vars);
 
-    // Run the server until stopped.
-    s.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "exception: " << e.what() << "\n";
-  }
+	std::ifstream config(configFile.c_str());
+	if (config) {
+		po::store(po::parse_config_file(config, server_config), config_vars);
+		po::notify(config_vars);
+	}
+	
+	if (config_vars.count("help")) {
+		std::cout << server_config << std::endl;
+		return 0;
+	}
 
-  return 0;
+	if (port == 0 || host.empty() || docsRoot.empty()) {
+		std::cout << "Not enough parameters to start reminder http server" << std::endl << std::endl;
+		std::cout << server_config << std::endl;
+		return 0;
+	}
+
+	try
+	{
+		// Initialise the server.
+		http::server::server s(host, std::to_string(port), docsRoot);
+
+		// Run the server until stopped.
+		s.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "exception: " << e.what() << "\n";
+	}
+
+	return 0;
 }
